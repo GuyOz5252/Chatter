9f1c38900c7ce354e65e1ddf7880645545e37a39
Guy Oz <109523947+GuyOz5252@users.noreply.github.com>
2025-05-29 23:15:16 +0300

    Added services

--- src/Chatter.Api/Program.cs
+++ src/Chatter.Api/Program.cs
@@ -1,15 +1,48 @@
+using System.Reflection;
+using Chatter.Api.Middlewares;
+using Chatter.Application.Chats.Create;
+using Chatter.Application.Chats.Get;
+using Chatter.Application.Chats.SendChatMessage;
+using Chatter.Application.Users.Register;
+using Chatter.Domain.Abstract;
+using Chatter.Domain.Entities;
+using Chatter.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Serilog;
+using SharedKernel;
+using SharedKernel.Messaging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((context, loggerConfig) =>
     loggerConfig.ReadFrom.Configuration(context.Configuration));
 
-// builder.Services
-//     .AddApplication()
-//     .AddInfrastructure()
-//     .AddApi;
+builder.Services.AddDbContext<ApplicationDbContext>(optionsBuilder =>
+{
+    optionsBuilder.UseInMemoryDatabase("ChatterDb");
+});
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IChatRepository, ChatRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IDateTimeProvider, DateTimeProvider>();
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+builder.Services.AddScoped<ICommandHandler<RegisterUserCommand, Guid>,  RegisterUserCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<CreateChatCommand, Guid>, CreateChatCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<SendChatMessageCommand>, SendChatMessageCommandHandler>();
+builder.Services.AddScoped<IQueryHandler<GetChatsQuery, List<Chat>>, GetChatsQueryHandler>();
+
+builder.Services.AddProblemDetails();
+builder.Services.AddExceptionHandler<ProblemDetailsExceptionHandler>();
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Chatter API" });
+});
 
 var app = builder.Build();
+
+app.UseSwaggerUI();
+app.UseExceptionHandler();
 
 await app.RunAsync();
